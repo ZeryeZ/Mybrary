0c77bb8bf0cec31a12eb4938092d5f9ffdf96897
ZeryeZ <thorben.staubach@icloud.com>
2021-06-30 12:16:33 +0200

    second tutorial 20min

--- server.js
+++ server.js
@@ -6,15 +6,19 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 
 const indexRouter = require('./routes/index.js')
+const authorRouter = require('./routes/authors.js')
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -25,5 +29,7 @@ db.on('connected', () => console.log('working '))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 
 app.listen(process.env.PORT || 3000);
