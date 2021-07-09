98b636fcc08d45f9343da832d750e8044128f7c3
ZeryeZ <thorben.staubach@icloud.com>
2021-07-09 14:39:53 +0200

    With description for understanding

--- routes/authors.js
+++ routes/authors.js
@@ -1,28 +1,43 @@
+
+/* import = require which means this imports express and from my own project files from models/authors.js
+    then it sets express.Router equal to router
+*/
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/authors.js')
 
+
 // All authors route
+// router is a middleware same as app but one instance under app its there to handle the req and res.
 router.get('/', async (req, res) => {
-    let searchOptions = {}
+    
+    let searchOptions = {};
+//  req.query.name ist was req= nachfrage query= abfrage nach etwas name= is the self made variable which is set equal to whatever you typed in
+//  RegExp creates a string with extras which allows you to search through a string and filter things youre looking for out of it 
     if (req.query.name != null && req.query.name !== '') {
-        searchOptions.name = new RegExp(req.query.name, 'i')
+
+        searchOptions = new RegExp(req.query.name, 'i')
+        
     }
     try {
-        const authors = await Author.find({searchOptions})
+        const authors = await Author.find(searchOptions);
+        // console.log(authors);  
         res.render('authors/index', { 
             authors: authors,
             searchOptions: req.query 
         })
     } catch {
+        // console.log("is redirecting");
         res.redirect('/')
     }
     
 })
 
+
 // New Author Route
 router.get('/new', (req, res) => {
     res.render('authors/new', { author: new Author() })
+    
 })
 
 // Create Author Route
@@ -30,6 +45,7 @@ router.post('/', async (req, res) => {
     const author = new Author({
         name: req.body.name
     })
+    console.log(author);
     try {
         const newAuthor = await author.save()
     //  res.redirect(`authors/${newAuthor.id}`)
--- server.js
+++ server.js
@@ -12,24 +12,39 @@ const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index.js')
 const authorRouter = require('./routes/authors.js')
 
-
+// set preferences for App/Website
 app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
+
+// where to find Views 
+app.set('views', __dirname + '/views') 
+
+// where to find Layouts
 app.set('layout', 'layouts/layout')
+
+// which layout type to use
 app.use(expressLayouts)
+// sets folder public to static type every file which is in there will be of type static and that way not changeable on runtime (i guess)
 app.use(express.static('public'))
+
+// extended: sets nested(verschachtelt) to false so json files cant be nested inside themselves anymore and limit sets the size of these files to 10 megabytes
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
-
+// Setups the Databank
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
 const db = mongoose.connection;
+
+// Handles error 
 db.on('error', error => console.log(error))
+// tells you when its connected 
 db.once('open', () => console.log('connected to Mongoose'))
 db.on('connected', () => console.log('working '))
 
 
+// App uses / indexRouter file and for /authors the authorRouter file
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
 
+// app listens to port of process.env.PORT which is a var that can be changed and if there is non in there it will take 3000 
 
 app.listen(process.env.PORT || 3000);
